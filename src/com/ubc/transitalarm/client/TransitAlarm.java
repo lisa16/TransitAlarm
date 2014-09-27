f166abad6135e477e6e3985962bee22b626156ff
Lisa Wong <miss.lisa19@hotmail.com>
2014-09-27 16:43:12 -0700

    fixed the refresh method

--- src/com/ubc/transitalarm/client/TransitAlarm.java
+++ src/com/ubc/transitalarm/client/TransitAlarm.java
@@ -13,6 +13,7 @@ import com.google.gwt.geolocation.client.PositionError;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -37,17 +38,36 @@ public class TransitAlarm implements EntryPoint {
 	private double longitude;
 	private int queryInterval = 10000; 
 	private int refreshInterval = 10000;
+	Geolocation geoposition; 
+	Geolocation.PositionOptions options;
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		Geolocation geoposition = Geolocation.getIfSupported();
+		refreshPosition();
+
+
+		Timer refreshTimer = new Timer() {
+			public void run() {
+				refreshPosition();
+			}
+		};
+		refreshTimer.scheduleRepeating(refreshInterval); // Auto refresh every 10 secs
+
+		Button refreshButton = new Button ("Refresh");
+		refreshButton.getElement().setClassName("btn btn-info");
+		refreshButton.addClickHandler(new refreshClickHandler());
+		RootPanel.get().add(refreshButton);
+	}
+
+	private void refreshPosition() {
+		geoposition = Geolocation.getIfSupported();
 		if (geoposition == null) {
 			Window.alert("Sorry, your browser doesn't support the Geolocation feature!");
 		}
 
-		Geolocation.PositionOptions options = new PositionOptions();
+		options = new PositionOptions();
 		options.setMaximumAge(queryInterval);
 
 		geoposition.getCurrentPosition(new Callback<Position, PositionError>() {
@@ -56,30 +76,22 @@ public class TransitAlarm implements EntryPoint {
 				Coordinates coordinates = result.getCoordinates();
 				latitude = coordinates.getLatitude();
 				longitude = coordinates.getLongitude();
+				System.out.println(latitude);
+				System.out.println(longitude);
 			}
 
 			@Override
 			public void onFailure(PositionError reason) {
 				Window.alert("Sorry, your location cannot be determined!");
 			}
-		});	
+		}, options);
 
-		Timer refreshTimer = new Timer() {
-			public void run() {
-				Window.Location.reload();
-			}
-		};
-		refreshTimer.scheduleRepeating(refreshInterval); // Auto refresh every 10 secs
-		
-		Button refreshButton = new Button ("Refresh");
-		refreshButton.getElement().setClassName("btn btn-info");
-		refreshButton.addClickHandler(new refreshClickHandler());
 	}
-	
+
 	class refreshClickHandler implements ClickHandler{
 		@Override
 		public void onClick(ClickEvent event) {
-			Window.Location.reload();				
+			refreshPosition();
 		}			
 	}
 }
