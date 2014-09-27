5278af97ccd5ab0ffe250d715841878dbab3583a
Eric Kim <bosung90@gmail.com>
2014-09-27 16:48:05 -0700

    added alarm page UI, added button on the first page that directs user to the second page.

--- src/com/ubc/transitalarm/client/TransitAlarm.java
+++ src/com/ubc/transitalarm/client/TransitAlarm.java
@@ -2,6 +2,11 @@ package com.ubc.transitalarm.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -20,12 +25,83 @@ public class TransitAlarm implements EntryPoint {
 	 */
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
+	final HTML alarmPageHTML = new HTML();
+	final HTML destinationPageHTML = new HTML();
+	
 	/**
 	 * This is the entry point method.
 	 */
-	public void onModuleLoad() {
+	public void onModuleLoad() 
+	{
+		destinationPageHTML.setHTML("		<div class=\"row\">\r\n" + 
+				"			<h3>Source:</h3>\r\n" + 
+				"			<input class=\"form-control\" placeHolder=\"Enter Starting Location\"\r\n" + 
+				"				type=\"text\"></input> <input type=\"checkbox\"> Current\r\n" + 
+				"			Location\r\n" + 
+				"		</div>\r\n" + 
+				"\r\n" + 
+				"		<div class=\"row\">\r\n" + 
+				"			<h3>Dest:</h3>\r\n" + 
+				"			<input class=\"form-control\" placeHolder=\"Enter Destination Location\"\r\n" + 
+				"				type=\"text\"></input>\r\n" + 
+				"		</div>\r\n" + 
+				"\r\n" + 
+				"		<button type=\"button\" class=\"btn btn-lg btn-primary\">Search</button>");
+		
+		alarmPageHTML.setHTML("		<div class=\"row\">\r\n" + 
+				"			<button type=\"button\" class=\"btn btn-lg btn-primary\">Stop Alarm</button>\r\n" + 
+				"		</div>\r\n" + 
+				"\r\n" + 
+				"		<div class=\"row\">\r\n" + 
+				"			<h3>Transfer stop name 1:</h3>\r\n" + 
+				"			<h4>Distance remaining <font color=\"red\">500m</font></h4>\r\n" + 
+				"		</div>\r\n" + 
+				"\r\n" + 
+				"		<div class=\"row\">\r\n" + 
+				"			<h3>Transfer stop name 2:</h3>\r\n" + 
+				"			<h4>Distance remaining 10km</h4>\r\n" + 
+				"		</div>\r\n" + 
+				"\r\n" + 
+				"		<div class=\"row\">\r\n" + 
+				"			<h3>Transfer stop name 3:</h3>\r\n" + 
+				"			<h4>Distance remaining 15km</h4>\r\n" + 
+				"		</div>\r\n" + 
+				"\r\n" + 
+				"		<div class=\"row\">\r\n" + 
+				"			<button type=\"button\" class=\"btn btn-lg btn-primary\">Refresh Now</button>\r\n" + 
+				"			<h4>refreshing in 40 seconds...</h4>\r\n" + 
+				"		</div>");
+		
+		
+		loadDestinationPage();
+//		loadAlarmPage();
+		
+	}
+	
+	public void loadDestinationPage()
+	{
+		Button searchButton = new Button("Search");
+		searchButton.getElement().setClassName("btn btn-lg btn-primary");
+		searchButton.addClickHandler(new SearchButtonClickHandler());
+		
+		RootPanel.get("searchDestinationField").add(destinationPageHTML);
+		RootPanel.get("searchDestinationField").add(searchButton);
+	}
+	
+	public void loadAlarmPage()
+	{
+		System.out.println("Loading alarm page");
+		RootPanel.get("alarmPageField").add(alarmPageHTML);
+	}
+	
+	class SearchButtonClickHandler implements ClickHandler {
 
+		public void onClick(ClickEvent event) 
+		{
 
-
+			RootPanel.get("searchDestinationField").clear();
+			
+			loadAlarmPage();
+		}
 	}
 }
