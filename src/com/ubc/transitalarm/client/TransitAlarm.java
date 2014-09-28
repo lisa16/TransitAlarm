fb8a5f85593c14112f49c0d04b3d3f0aad37e97f
Alborz Massah <alborzmassah@gmail.com>
2014-09-27 17:38:41 -0700

    adding calculate distance between two lat and long

--- src/com/ubc/transitalarm/client/TransitAlarm.java
+++ src/com/ubc/transitalarm/client/TransitAlarm.java
@@ -42,6 +42,15 @@ public class TransitAlarm implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		// example distance calculations by Alborz
+		/*
+		 System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, 'M') + " Miles\n");
+	      System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, 'K') + " Kilometers\n");
+	      System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, 'N') + " Nautical Miles\n");
+		System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, 'Meter') + " Meters\n");
+*/
+		
+	
 		Geolocation geoposition = Geolocation.getIfSupported();
 		if (geoposition == null) {
 			Window.alert("Sorry, your browser doesn't support the Geolocation feature!");
@@ -82,4 +91,40 @@ public class TransitAlarm implements EntryPoint {
 			Window.Location.reload();				
 		}			
 	}
-}
+	
+    private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == 'K') {
+          dist = dist * 1.609344;}
+        else if (unit == 'Q'){
+        	  dist = (( dist * 1.609344 ) / 1000);
+          }
+        else if (unit == 'N') {
+          dist = dist * 0.8684;
+          }
+        return (dist);
+      }
+
+      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+      /*::  This function converts decimal degrees to radians             :*/
+      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+      private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+      }
+
+      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+      /*::  This function converts radians to decimal degrees             :*/
+      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+      private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+      }}
+
+     
+	
+		
+	
+
